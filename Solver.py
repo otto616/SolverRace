67840df89ad31d2df288b7f8c5d1e6a3c7ca2f7d
otto616 <ottobielrodenas@gmail.com>
2026-03-02 17:20:47 +0100

    init

---
+++ Solver.py
@@ -0,0 +1,62 @@
+import sys, os, random
+
+# data structure to store the CNF
+class CNF:
+    def __init__(self):
+        self.variables = 0
+        self.num_clauses = 0
+        self.clauses = []
+
+
+def parse_input(cnf_file):   
+    cnf = CNF()
+    with open(cnf_file, 'r') as f:
+        add_clause = cnf.clauses.append
+        for line in f:
+
+            # skip comments
+            if line.startswith('c'):
+                continue
+
+            # parse the header
+            if line.startswith('p'):
+                parts = line.split()
+                cnf.variables = int(parts[2])
+                cnf.num_clauses = int(parts[3])
+                continue
+
+            parts = line.split()
+
+            # skip empty lines
+            if not parts:
+                continue
+
+            # parse the clauses with variables
+            add_clause(list(map(int, parts[:-1])))
+                
+    return cnf
+
+
+class Solver:
+    def __init__(self, cnf):
+        self.cnf = cnf
+        self.assignment = []
+        self.var_to_clause = []
+
+    # assign random values to variables
+    def assign(self):
+        self.assignment = [None] + [random.getrandbits(1) for _ in range(self.cnf.variables)]
+
+    def evaluate(self):
+        return sum(
+            any((literal > 0) == self.assignment[abs(literal)] for literal in clause)
+            for clause in self.cnf.clauses
+        )
+
+    def initialize_state():
+
+
+        
+
+    
+
